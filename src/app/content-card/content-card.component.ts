6a2e8712e9ffdbf3f2020f47ac23556c0edaab64
Christian-DAntonio <CHRISTIAN.DANTONIO@stclairconnect.ca>
2023-05-14 15:08:27 -0400

    ALmost forgot some changes, now I'm done

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -14,28 +14,28 @@ export class ContentCardComponent {
     description: 'The compelling sequel to "The Godfather," contrasting the life of Corleone father and son. Traces the problems of Michael Corleone (Al Pacino) in 1958 and that of a young immigrant Vito Corleone (Robert De Niro) in 1917s Hells Kitchen. Michael survives many misfortunes and Vito is introduced to a life of crime.',
     creator: 'Francis Ford Coppola',
     genre: 'Crime',
-    body: '',
+    body: 'Should be obivous enough as to why this one is here',
     releaseDate: 1974
   }
 
   contentItem2:Content = {
-    id: 1024,
-    title: 'hello',
-    description: '',
-    creator: '',
-    genre: '',
-    body: '',
-    releaseDate: 1974
+    id: 1025,
+    title: 'The Hunt',
+    description: 'A kindergarten teachers (Mads Mikkelsen) world collapses around him after one of his students (Annika Wedderkopp), who has a crush on him, implies that he committed a lewd act in front of her.',
+    creator: 'Thomas Vinterberg',
+    genre: 'Drama',
+    body: 'This one is probably my favourite movie out of the three',
+    releaseDate: 2012
   }
 
   contentItem3:Content = {
-    id: 1024,
-    title: 'hello',
-    description: '',
-    creator: '',
-    genre: '',
-    body: '',
-    releaseDate: 1974
+    id: 1026,
+    title: 'Sunset Boulevard',
+    description: 'An aging silent film queen refuses to accept that her stardom has ended. She hires a young screenwriter to help set up her movie comeback. The screenwriter believes he can manipulate her, but he soon finds out he is wrong. The screenwriters ambivalence about their relationship and her unwillingness',
+    creator: 'Bily Wilder',
+    genre: 'Drama',
+    body: 'Good movie',
+    releaseDate: 1950
   }
   myContentList: ContentList;
 
