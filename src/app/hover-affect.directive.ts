5711527e64e65f779faf7ac21fb7c860d7027e25
Christian-DAntonio <CHRISTIAN.DANTONIO@stclairconnect.ca>
2023-06-04 08:46:19 -0400

    more things have changed

--- src/app/hover-affect.directive.ts
+++ src/app/hover-affect.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
-export class HoverAffectDirective {
-
-  constructor() { }
-
+export class HoverAffectDirective implements OnInit {
+  @Input () colour: string;
+  constructor(private elm: ElementRef) { 
+  }
+  @HostListener('click') onClick() {
+    this.highlightMe(this.colour);
+  }
+  private highlightMe(highlightColour: string) {
+    this.elm.nativeElement.style.backgroundColor = highlightColour || 'green';
+  }
+  ngOnInit(): void{
+    this.elm.nativeElement.style.backgroundColor = this.colour || "green";
+  }
 }
