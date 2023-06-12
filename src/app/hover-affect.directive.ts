3ef3e4ce7db66551aa25362d64475b800d074391
Christian-DAntonio <CHRISTIAN.DANTONIO@stclairconnect.ca>
2023-06-12 16:39:53 -0400

    package.json updated

--- src/app/hover-affect.directive.ts
+++ src/app/hover-affect.directive.ts
@@ -1,24 +1,52 @@
-import { Directive, ElementRef, Host, HostListener, OnInit, Input } from '@angular/core';
+import { Directive, ElementRef, Host, HostListener, OnInit, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective implements OnInit {
   @Input () colour: string;
-  constructor(private elm: ElementRef) { 
+  @Input () affectName: string;
+  constructor(private elm: ElementRef, renderer: Renderer2) { 
     this.elm.nativeElement.style.backgroundColor = 'yellow';
   }
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('yellow');
+    switch(this.affectName) {
+      case 'bold':
+        this.bold(true);
+        break;
+      case 'underline':
+        this.underline(true);
+        break;
+    }
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.highlight('');
+    switch (this.affectName) {
+      case 'bold':
+        this.bold(false);
+        break;
+      case 'underline':
+        this.bold(false);
+        break;
+    }
   }
 
-  private highlight(colour: string) {
-    this.elm.nativeElement.style.backgroundColor = colour;
+  private bold(bold:boolean){
+    if(bold){
+      this.elm.nativeElement.style.fontWeight = 700;
+    } else {
+      this.elm.nativeElement.style.fontWeight = 20;
+    }
   }
+
+  private underline(underline:boolean){
+    if(underline){
+      this.elm.nativeElement.style.underline = this.colour || "green";
+    } else {
+      this.elm.nativeElement.style.underline = 'black';
+    }
+  }
+9
    ngOnInit(): void{
     this.elm.nativeElement.style.backgroundColor = this.colour || "";
   } 
